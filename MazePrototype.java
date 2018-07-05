825aeb577064d1cc7f3251677df40286981a97dc
rasuyas <rasuyas@gmail.com>
2018-07-05 11:12:27 +0700

    Maze Prototype

---
+++ MazePrototype.java
@@ -0,0 +1,15 @@
+public class ProjectSADjikstra {
+	static int[][] maze1;
+	
+	public static void main(String[] args) {
+        // TODO code application logic here
+		maze1 = new int[][]{{0,0,0,0,0,0,1,0},
+                            {0,0,1,0,0,0,1,0},
+                            {0,1,1,1,1,1,1,0},
+                            {0,1,0,0,0,1,0,0},
+                            {0,1,1,1,1,1,0,0},
+                            {0,1,0,1,0,0,1,0},
+                            {0,1,1,1,1,1,1,0},
+                            {0,1,0,0,0,0,0,0}};
+    }
+}
